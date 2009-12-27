a3adc65b68ea8594ccfe2e901353a6d056e799b3
yang.nick.baoguang <yang.nick.baoguang@gmail.com>
2009-12-27 08:40:12 +0000

    Camera update

--- Include/S3UT/S3UTcamera.h
+++ Include/S3UT/S3UTcamera.h
@@ -1,19 +1,11 @@
 //----------------------------------------------------------------------------------
-// File:   S3UTCamera.h
-// Email:  sdkfeedback@S3Graphics.com
+// File:   S3UTcamera.h
+// Author: Baoguang Yang
 // 
-// Copyright (c) 2007 S3Graphics Corporation. All rights reserved.
+// Copyright (c) 2009 _COMPANYNAME_ Corporation. All rights reserved.
 //
-// TO  THE MAXIMUM  EXTENT PERMITTED  BY APPLICABLE  LAW, THIS SOFTWARE  IS PROVIDED
-// *AS IS*  AND S3Graphics AND  ITS SUPPLIERS DISCLAIM  ALL WARRANTIES,  EITHER  EXPRESS
-// OR IMPLIED, INCLUDING, BUT NOT LIMITED  TO, IMPLIED WARRANTIES OF MERCHANTABILITY
-// AND FITNESS FOR A PARTICULAR PURPOSE.  IN NO EVENT SHALL  S3Graphics OR ITS SUPPLIERS
-// BE  LIABLE  FOR  ANY  SPECIAL,  INCIDENTAL,  INDIRECT,  OR  CONSEQUENTIAL DAMAGES
-// WHATSOEVER (INCLUDING, WITHOUT LIMITATION,  DAMAGES FOR LOSS OF BUSINESS PROFITS,
-// BUSINESS INTERRUPTION, LOSS OF BUSINESS INFORMATION, OR ANY OTHER PECUNIARY LOSS)
-// ARISING OUT OF THE  USE OF OR INABILITY  TO USE THIS SOFTWARE, EVEN IF S3Graphics HAS
-// BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-//
+// Cameras are also considered as lights, so they have light properties
+// like m_bCastShadow, m_fLightSize
 //
 //----------------------------------------------------------------------------------
 
@@ -21,21 +13,46 @@
 #ifndef S3UTCAMERA_H
 #define S3UTCAMERA_H
 
+#include <string>
 #include <dxutcamera.h>
+
+using namespace std;
 
 class S3UTCamera : public CModelViewerCamera
 {
 public:
-    // automatically set near and far planes according to object AABB
-    // note that function uses view matrix passed from the outside, not the one
-    // stored inside the camera
+	enum CameraType
+	{
+		eLight,
+		eEye,
+		eUnknown
+	};
+
     virtual void SetProjParams(FLOAT fFOV, FLOAT fAspect, D3DXMATRIX mView, D3DXVECTOR3 vBBox[2]);
     void SetProjParams(D3DXMATRIX mView, D3DXVECTOR3 vBBox[2]);
 	void SetProjParams(FLOAT fFOV, FLOAT fAspect, FLOAT fNear,	FLOAT fFar);
 	void OnKeyboard(UINT nChar, bool bKeyDown, bool bAltDown, void* pUserContext);
+    void SetModelRot(const D3DXMATRIX &m) { m_mModelRot = m; }
 
-    void SetModelRot(const D3DXMATRIX &m)
-    { m_mModelRot = m; }
+	CameraType GetCamType()	const { return m_eCamType; };
+	void SetCamType( CameraType camType ) { m_eCamType = camType; };
+
+	string GetCamName() const { return m_sName; };
+	void SetCamName( const string& camName ) { m_sName = camName; };
+	void SetCamName( const char *camName ) { m_sName = string( camName ); };
+
+	bool IsCastShadow() const { return m_bCastShadow; };
+	void SetCastShadow( bool bCastShadow ) { m_bCastShadow = bCastShadow; };
+
+	float GetLightSize() const { return m_fLightSize; };
+	void  SetLightSize( float lightSize ) { m_fLightSize = lightSize; };
+
+
+private:
+	CameraType	m_eCamType;
+	string		m_sName;
+	bool		m_bCastShadow;
+	float		m_fLightSize;
 };
 
 #endif // S3UTCAMERA_H
