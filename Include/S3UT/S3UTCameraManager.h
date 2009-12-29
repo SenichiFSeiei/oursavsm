e599d094afb91d4382131a295549ca7d4655e276
yang.nick.baoguang <yang.nick.baoguang@gmail.com>
2009-12-29 05:58:58 +0000

    Add UI handle code to camera manager

--- Include/S3UT/S3UTCameraManager.h
+++ Include/S3UT/S3UTCameraManager.h
@@ -21,12 +21,20 @@ class S3UTCamera;
 
 class S3UTCameraManager
 {
+	static const int HANDLE_BASE = 10000;
 public:
 	S3UTCameraManager(){};
 	~S3UTCameraManager();
+	
 	void ConfigCameras( char *fileName );
 	void DumpCameraStatus( char *fileName ) const;
+	void SetupCameraUI( CDXUTDialog &camUI ) const;
+	void SyncToCameraUI(CDXUTDialog &camUI);
+	
+	S3UTCamera *Camera( int num );
 	S3UTCamera *Eye( int num );//start counting from 0
+	S3UTCamera *ActiveEye();//the 1st active eye camera.
+	int CameraCount() const { return static_cast<int>(m_aPtrCameras.size()); };
 
 	void OnD3D10SwapChainResized( ID3D10Device* pDev10, IDXGISwapChain *pSwapChain, const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc, void* pUserContext );
 
